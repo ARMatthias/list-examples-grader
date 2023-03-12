22749c716b140cc53e7e0b08398bd538a52e63e2
ARMatthias <amatthias@ucsd.edu>
2023-03-11 23:11:15 -0800

    annotated w/ comments for clarity

--- grade.sh
+++ grade.sh
@@ -1,32 +1,48 @@
+#!/bin/bash
+#
+# grades a student submission ListExamples.java using an input github link to the submission repositiory.
+#
+# Name: Rae Matthias
+# Email: amatthias@ucsd.edu
+# PID: A17495586
+
+# classpath config depending on operating system
 if [[ $OSTYPE == "linux-gnu" ]] 
-    then 
+    then
         CPATH=".:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar"
 elif [[ $OSTYPE == "msys" ]] 
     then
         CPATH=".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar"
 fi
 
+# clear previous submission
 rm -rf student-submission
 truncate -s 0 gradeReports/*.txt
-
 if [[ -e ListExamples.java ]]
+    # if exists, remove previous ListExamples.java
     then 
         rm ListExamples.java
 fi
 
+# retrieve student submission
 git clone $1 student-submission
 
+# check if ListExamples.java is in student submission
 if [[ -e student-submission/ListExamples.java ]]
+    # if exists, print found, copy student submission out
     then
         echo "ListExamples.java found"
         cp student-submission/ListExamples.java ./
 else
+    # if not, print error, exit
     echo "Error: File ListExamples.java not found"
     exit 1
 fi
 
+# compile and check if compilation is successful
 javac -cp $CPATH *.java 2> gradeReports/javac-errs.txt
 if [[ $? -ne 0 ]] 
+    # if unsuccessful, print standard error, exit
     then 
         echo "Error: failure during compilation"
         echo "Compilation feedback:"
@@ -34,15 +50,19 @@ if [[ $? -ne 0 ]]
         exit 1
 fi
 
+# run and record outputs to corresponding files
 java -cp $CPATH org.junit.runner.JUnitCore TestListExamples 2> gradeReports/run-err.txt 1> gradeReports/jUnit-out.txt
 OUT=$(cat gradeReports/run-err.txt)
 
+# check for successful runtime
 if [[ $? -ne 0 ]]
+    # if unsuccessful, print standard error, exit
     then
         echo "Error: failure during runtinme"
         echo $OUT
         exit 1
 else
+    #if successful, grade based off of total passed tests of TestListExamples.java
     if grep -q "FAILURES" gradeReports/jUnit-out.txt;
         then 
             cat gradeReports/jUnit-out.txt
--- testGrade.sh
+++ testGrade.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+#
+# Tests grade.sh using 7 example student submissions (links stored in links.txt)
+#
+# Name: Rae Matthias
+# Email: amatthias@ucsd.edu
+# PID: A17495586
+
+# classpath config depending on operating system
 if [[ $OSTYPE == "linux-gnu" ]] 
     then 
         CPATH=".:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar"
@@ -6,11 +15,12 @@ elif [[ $OSTYPE == "msys" ]]
         CPATH=".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar"
 fi
 
+# makes an array of the titles of the cases, in the order they will be tested
 TITLES="Original (Lab 3):Correct:Compile Error:Incorrect Method Signature:Incorrect Filename:Nested File:Subtle Error"
 IFS=":" read -a CASES <<< $TITLES
 
+# detemines which set of links to use (ssh(git@git) or local(https))
 echo $SHH_CLIENT > gradeReports/javac-errs.txt
-
 if grep -q "." gradeReports/javac-errs.txt;
     then 
         LIST=$(grep "git@git" links.txt)
@@ -18,32 +28,40 @@ else
     LIST=$(grep "https" links.txt)
 fi
 
+# makes an array of the links of the cases, in the order they will be tested
 IFS="&" read -a LINKS <<< $LIST
 
+# runs grade.sh for each case, labeling each for their respective case in terminal output
 for i in ${!LINKS[@]}; do
     echo "============= ${CASES[$i]} ============="
 
+    # clear previous submission
     rm -rf student-submission
     truncate -s 0 gradeReports/*.txt
-
     if [[ -e ListExamples.java ]]
         then 
             rm ListExamples.java
     fi
 
+    # retrieve student submission
     git clone ${LINKS[$i]} student-submission
 
+    # check if ListExamples.java is in student submission
     if [[ -e student-submission/ListExamples.java ]]
+        # if exists, print found, copy student submission out
         then
             echo "ListExamples.java found"
             cp student-submission/ListExamples.java ./
     else
+        # if not, print error, continue to next case
         echo "Error: File ListExamples.java not found"
         continue
     fi
 
+    # compile and check if compilation is successful
     javac -cp $CPATH *.java 2> gradeReports/javac-errs.txt
     if [[ $? -ne 0 ]] 
+        # if unsuccessful, print standard error, continue to next case
         then 
             echo "Error: failure during compilation"
             echo "Compilation feedback:"
@@ -51,15 +69,19 @@ for i in ${!LINKS[@]}; do
             continue
     fi
 
+    # run and record outputs to corresponding files
     java -cp $CPATH org.junit.runner.JUnitCore TestListExamples 2> gradeReports/run-err.txt 1> gradeReports/jUnit-out.txt
     OUT=$(cat gradeReports/run-err.txt)
 
+    # check for successful runtime
     if [[ $? -ne 0 ]]
+        # if unsuccessful, print standard error, continue to next case
         then
             echo "Error: failure during runtinme"
             echo $OUT
             continue
     else
+        #if successful, grade based off of total passed tests of TestListExamples.java
         if grep -q "FAILURES" gradeReports/jUnit-out.txt;
             then 
                 cat gradeReports/jUnit-out.txt
